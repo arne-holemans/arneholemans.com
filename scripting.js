93510ce47fad67faa5d3645b86d5c56d20cca03c
arne-holemans <90710876+arne-holemans@users.noreply.github.com>
2023-11-23 21:12:58 +0100

    new version

--- scripting.js
+++ scripting.js
@@ -1,17 +1,27 @@
-function switchLanguage(language) {
-    document.querySelectorAll('[lang]').forEach(element => {
-      if (element.getAttribute('lang') === language) {
-        element.style.display = 'block';
-      } else {
-        element.style.display = 'none';
-      }
-    });
-  }
 
-  function toggleDropdown() {
-    var dropdown = document.querySelector('.dropdown-content');
-    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
-  }
+//dropdown menu for mobile
+function toggleDropdown() {
+  var dropdown = document.querySelector('.dropdown-content');
+  dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
+}
 
-  // Initially, hide content in Dutch
-  switchLanguage('en');
+// Function to get the current date in the format YYYY-MM-DD
+function getCurrentDate() {
+  const today = new Date();
+  const year = today.getFullYear();
+
+  return `${year}`;
+}
+
+// Function to update the content of the currentDate span
+function updateCurrentDate() {
+  const currentDateElement = document.getElementById('currentYear');
+  if (currentDateElement) {
+    currentDateElement.textContent = getCurrentDate();
+  }
+}
+
+// Call the updateCurrentDate function when the page loads
+window.onload = function () {
+  updateCurrentDate();
+};
